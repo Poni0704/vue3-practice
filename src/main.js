1d86c54d037cdfc36cc874fc3e59b3585eecb331
Poni <a0938659639@gmail.com>
2025-04-14 00:46:36 +0800

    add 學習component&如何拆分component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App2 from "./App2.vue";
+import router from "./router";
+// import "@/assets/css/reset.css";
 
-createApp(App).use(router).mount('#app')
+// use(xxx) => 使用vue以外的擴充組件
+createApp(App2).use(router).mount("#app");
